69c4ab256839c079a67fe4dee1982a28c41fc6cd
TJ Epperson <tj.epperson070@gmail.com>
2024-08-17 17:36:11 +0900

    Add progress component

--- src/components/Progress.svelte
+++ src/components/Progress.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
-	const { type, value }: { type: 'linear'; value: number } = $props();
+	const { type = 'linear', progress }: { type?: 'linear'; progress: string } = $props();
 </script>
 
-<div class="loader"></div>
+<div class="wrapper">
+	<div class="bar" style={`width: ${progress}%`}></div>
+</div>
 
 <style lang="scss" scoped>
-	.loader {
+	.wrapper {
+		width: 100%;
+		height: 10px;
+		background-color: rgba(0, 0, 0, 0.2);
+		border-radius: 10px;
+		overflow: hidden;
+
+		.bar {
+			height: 100%;
+			background-color: #3b82f6;
+			width: 0%;
+			transition: width 1.25s ease-in-out;
+			border-radius: 10px;
+		}
 	}
 </style>

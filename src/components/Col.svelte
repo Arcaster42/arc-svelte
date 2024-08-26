dc28def43d8f171073f0624b2534054bd86d467e
TJ Epperson <tj.epperson070@gmail.com>
2024-08-26 22:47:19 +0900

    Add Col, Row components

---
+++ src/components/Col.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	const { children }: { children?: Snippet } = $props();
+</script>
+
+<div class="col">
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
+
+<style lang="scss" scoped>
+	.col {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+		padding: 4px;
+	}
+</style>
---
+++ src/components/Row.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	const { children }: { children?: Snippet } = $props();
+</script>
+
+<div class="row">
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
+
+<style lang="scss" scoped>
+	.row {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+		width: 100%;
+		padding: 4px;
+	}
+</style>

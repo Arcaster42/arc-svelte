6ee5757e23547a4b3aadaf83468318cea70fbc0c
TJ Epperson <tj.epperson070@gmail.com>
2024-08-28 20:24:36 +0900

    Add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [svelte()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/lib/index.ts'),
+			name: 'ArcSvelteComponents',
+			fileName: 'arc-svelte-components'
+		},
+		rollupOptions: {
+			external: ['svelte'],
+			output: {
+				globals: {
+					svelte: 'Svelte'
+				}
+			}
+		}
+	}
 });

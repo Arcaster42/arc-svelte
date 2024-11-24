a0df159c93927d8cc73b56c22b7efdf31f1e13ad
TJ Epperson <tj.epperson070@gmail.com>
2024-11-24 14:07:26 +0900

    Add story for DatePicker

---
+++ src/lib/DatePicker.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+  import DatePicker from './DatePicker.svelte'
+
+  const { Story } = defineMeta({
+    title: 'Arc/DatePicker',
+    component: DatePicker,
+    tags: ['autodocs']
+  })
+</script>
+
+<Story name="Primary" args={{ label: 'DatePicker', color: 'primary' }} />
+<Story name="Range" args={{ label: 'DatePicker', color: 'primary', allowRange: true }} />
